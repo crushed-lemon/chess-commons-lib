a576f10ac2eac4aa1b85d0fa1d8da5df5485dce3
crushed-lemon <crushed.lemon.official@gmail.com>
2025-05-06 09:53:14 +0530

    Add a constructor to Move

--- src/main/java/com/crushedlemon/chess/commons/model/Move.java
+++ src/main/java/com/crushedlemon/chess/commons/model/Move.java
@@ -1,10 +1,12 @@
 package com.crushedlemon.chess.commons.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
+@AllArgsConstructor
 public class Move {
     Piece movedPiece;
     Color playerColor;
