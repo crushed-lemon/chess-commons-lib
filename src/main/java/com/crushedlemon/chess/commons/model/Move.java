1462a096e1733745ad247df8ee2636385ce801c8
crushed-lemon <crushed.lemon.official@gmail.com>
2025-05-06 09:58:24 +0530

    Add a default constructor to Move

--- src/main/java/com/crushedlemon/chess/commons/model/Move.java
+++ src/main/java/com/crushedlemon/chess/commons/model/Move.java
@@ -2,11 +2,13 @@ package com.crushedlemon.chess.commons.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @Value
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class Move {
     Piece movedPiece;
     Color playerColor;
